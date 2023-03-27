887f0e05a405d60e10702cb7a6c14547bebc16d3
Marcel <marcel_peruffo@brown.edu>
2023-03-27 17:40:19 +0200

    Update load_exo_shocks.m
    Simplified and removed ts stuff

--- Source/load_exo_shocks.m
+++ Source/load_exo_shocks.m
@@ -1,20 +1,10 @@
 %% load_exo_shocks.m
 
-% Loads exogenous shocks to hit the economy along the
-    % pf
-    % ts
-    % counterfactual pf
-    
+% Loads exogenous shocks to hit the economy
     
 %% Demand Shock    
-% Counterfactual
 dbe_cf = [0.01*[0.75.^[0:1:param.TTT-1]']];
     
-% % Eventual Exogenous Shock  
-% exo_shocks_pf = zeros(param.TTT*param.nX,1); 
-% % Endogenous Shocks
-% exo_shocks_PFC = zeros(param.TTT*(param.nX),1);
-
 
 
 
